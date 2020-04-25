fd442f9b66500b11c946b8682fd487d598beae87
Błażej <imnotfreeman@gmail.com>
2020-04-25 23:02:12 +0200

    added counting stats, bugfixes, code cleanup Co-authored-by: Ogro1337 <dawid.dopart@gmail.com>

--- main.py
+++ main.py
@@ -1,41 +1,68 @@
 from bitarray import bitarray
 import random
 
-BIT_ARRAY_LENGTH = 1000
-
-# Get a random number, between 0 and 100.
-# This number is the percentage of "1's" in out array.
-percentageOfOnes = random.randint(0,100)
-print(percentageOfOnes)
-
-mainData = bitarray(BIT_ARRAY_LENGTH)
-# Initially we set our array to contain only "0's".
-mainData.setall(False)
-
-# This loop's purpose is to allocate the "1's" in a random manner.
-iterationVar = ((mainData.length() * percentageOfOnes) / 100 )
-while(iterationVar > 0):
-    indexForOne = random.randint(0, mainData.length() - 1)
-    if mainData[indexForOne] == False:
-        mainData[indexForOne] = True
-        iterationVar = iterationVar - 1
-
-print(mainData)
-
-# scrambling here
-
-count3 = 0
-counter = 0
-#for num in range(1,999):
+BIT_ARRAY_LENGTH = 100
+PACKET_LENGTH = 100
 
 
-print(count3)
+def initArray(array): 
+    # Get a random number, between 0 and 100.
+    # This number is the percentage of "1's" in out array.
+    percentageOfOnes = random.randint(0,100)
 
-'''
-if mainData[num-1] != mainData[num]:
-    counter = 0
-else:
-    counter = counter + 1
-if counter == 2:
-    counter = -1
-    count3 = count3 + 1'''
+    #DEBUG!!!!!!
+    print("Procent: ", percentageOfOnes)
+
+    # Initially we set our array to contain only "0's".
+    array.setall(False)
+
+    # This loop's purpose is to allocate the "1's" in a random manner.
+    iterationVar = ((array.length() * percentageOfOnes) / 100 )
+    while(iterationVar > 0):
+        indexForOne = random.randint(0, array.length() - 1)
+        if array[indexForOne] is False:
+            array[indexForOne] = True
+            iterationVar = iterationVar - 1
+
+#Shows stats of a bitarray
+def getStats(array, statsArray):
+    currentIndex = 1
+    currentSeq = 1
+
+    #Couting every occurence 
+    while(currentIndex < len(array)):
+        if array[currentIndex] is array[currentIndex-1]:
+            currentSeq += 1
+        else: #If next is not the same, we save count current sequence
+            statsArray[currentSeq] += 1
+            currentSeq = 1
+
+        currentIndex += 1
+    statsArray[currentSeq] += 1     #Adding last sequence
+
+
+def main():
+    mainData = bitarray(BIT_ARRAY_LENGTH)
+    initArray(mainData)
+    ### DEBUG
+    print(mainData)
+
+    #initializing array with stats
+    statsArray = []
+    for num in range(0,PACKET_LENGTH+1):
+        statsArray.append(0)
+
+
+    getStats(mainData, statsArray)
+
+    #Showing stats (0 and 1 is not important)
+    currentIndex = 2
+    while(currentIndex < len(statsArray)):
+        if statsArray[currentIndex] is not 0:
+            print(currentIndex, " powtorzylo sie: ", statsArray[currentIndex])
+        currentIndex += 1
+
+
+if __name__ == "__main__":
+    main()
+
