0e7ae3903b1ea55f7bd66475fa82f0ee4083f0f7
Błażej <imnotfreeman@gmail.com>
2020-04-27 13:44:23 +0200

    filename changed, scrambling debug func in main

---
+++ xor_scrambler.py
@@ -0,0 +1,9 @@
+from bitarray import bitarray
+import random
+
+
+def XORScramble(array, scrambleKey):
+    currentIndex = 0
+    while(currentIndex < len(array)):
+        array[currentIndex:(currentIndex+len(scrambleKey))] = array[currentIndex:(currentIndex+len(scrambleKey))] ^ scrambleKey
+        currentIndex += len(scrambleKey)
