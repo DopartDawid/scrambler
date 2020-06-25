f99ea4194cd4ac8fe3b36de032e573883fbf0beb
sirgentleman <imnotfreeman@gmail.com>
2020-06-25 23:57:06 +0200

    Put boxplot and histogram on one plot

--- statsgen.py
+++ statsgen.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import seaborn as sns
 
 
 data_path = sys.argv[1]
@@ -21,9 +22,20 @@ quartiles = np.percentile(broken_packets, [25, 50, 75])
 max_value = np.max(broken_packets)
 min_value = np.min(broken_packets)
 
-fig, axs = plt.subplots(1, 2, tight_layout=True)
-axs[0].set_title('Boxplot of broken packets')
-axs[0].boxplot(broken_packets)
+sns.set(style="ticks")
+
+f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
+
+sns.boxplot(broken_packets, ax=ax_box)
+sns.distplot(broken_packets, ax=ax_hist)
+
+ax_box.set(yticks=[])
+sns.despine(ax=ax_hist)
+sns.despine(ax=ax_box, left=True)
+
+
+#plt.set_title('Boxplot of broken packets')
+#plt.boxplot(broken_packets)
 
 # GENERATE HIStOGRAM
 def function_model(x, mean, amplitude, std_deviation):
@@ -33,14 +45,15 @@ n_points = int(headers[0])
 n_bins = int(1 + np.floor(3.3*np.log(n_points)))
 
 
-n, bins, patches = axs[1].hist(broken_packets, bins=n_bins)
+n, bins, patches = plt.hist(broken_packets, bins=n_bins)
 bin_centers = bins[:-1] + np.diff(bins)/2
 params, _ = curve_fit(function_model, bin_centers, n, p0=[average, 0, deviation])
 x_interval_for_fit = np.linspace(bins[0], bins[-1], len(broken_packets))
-axs[1].plot(x_interval_for_fit, function_model(x_interval_for_fit, *params), '--')
-axs[1].set_title('Histogram of broken packets')
-axs[1].set_ylabel('Repetitions')
-axs[1].set_xlabel('Packets lost')
+plt.plot(x_interval_for_fit, function_model(x_interval_for_fit, *params), '--')
+
+plt.title('Histogram of broken packets')
+plt.ylabel('Repetitions')
+plt.xlabel('Packets lost')
 
 plt.savefig(sys.argv[2]+'.png')
 
@@ -54,4 +67,4 @@ with open(stats_file, 'w') as resultFile:
     resultFile.write('\n\tQ2 = ' + str(quartiles[1]))
     resultFile.write('\n\tQ3 = ' + str(quartiles[2]))
     resultFile.write('\n\tQ4 (maximum) = ' + str(max_value))
-    resultFile.write('\n\n\nFunction model\nMean = ' + str(params[0]) + '\nAmplitude = ' + str(params[1]) + '\nStandard deviation = ' + str(params[2]))
+    #resultFile.write('\n\n\nFunction model\nMean = ' + str(params[0]) + '\nAmplitude = ' + str(params[1]) + '\nStandard deviation = ' + str(params[2]))
